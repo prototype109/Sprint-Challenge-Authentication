4129947df41374e3184fd05f95e664bcad358921
Brandon Nichols <brandonaronnichols@gamil.com>
2019-12-13 20:26:10 -0600

    added jwt token generation and checks for login, will now test

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const secret = require("../secretCodes/secret");
 const User = require("../database/dbHelper");
 
 router.post("/register", async (req, res) => {
@@ -23,6 +24,37 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", (req, res) => {
   // implement login
+  try {
+    const user = User.getUser(req.body.username);
+    if (
+      user.length &&
+      bcrypt.compareSync(req.body.password, user[0].password)
+    ) {
+      const token = generateToken(user[0]);
+      res
+        .status(200)
+        .json({
+          message: `You have logged in as ${user[0].username}`,
+          token: token
+        });
+    } else {
+      res.status(401).json({ message: "incorrect credentials" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
+
+function generateToken(user) {
+  const payload = {
+    sub: user.username
+  };
+
+  const options = {
+    expiresIn: "1h"
+  };
+
+  jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
